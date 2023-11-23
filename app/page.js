b94fee0cccdbc9eec3fad9e66f69146985dc2875
foyle <foyle@spacecloud.kr>
2023-11-23 17:21:52 +0900

    test

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+
+  const handleRedirect = () => {
+    location.href = 'intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end'
+  }
   return (
     <main className={styles.main}>
       <div>
         TEST
-        <a href="intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end">TEST</a>
+        <a onClick={this.handleRedirect}>TEST</a>
       </div>
     </main>
   )
