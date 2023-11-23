772700dbc47efd8751a4b619481d60555103a43f
foyle <foyle@spacecloud.kr>
2023-11-23 17:16:23 +0900

    test

---
+++ app/test.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      TEST
+      <a href="intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end">TEST</a>
+    </div>
+  )
+}
