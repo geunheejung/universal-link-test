b4bf0d23b900f1bdb382fd5ef103475dc2a16ecd
foyle <foyle@spacecloud.kr>
2023-11-23 17:19:28 +0900

    test

--- app/test.js
+++ app/test.js
@@ -1,9 +1,6 @@
 
 export default function Home() {
   return (
-    <div>
-      TEST
-      <a href="intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end">TEST</a>
-    </div>
+   
   )
 }
