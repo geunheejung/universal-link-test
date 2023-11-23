7a5f71a4110bf096c81c7a3cd1e4f9a838f1629a
foyle <foyle@spacecloud.kr>
2023-11-23 17:26:05 +0900

    test

--- app/page.js
+++ app/page.js
@@ -3,14 +3,13 @@ import styles from './page.module.css'
 
 export default function Home() {
 
-  const handleRedirect = () => {
-    location.href = 'intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end'
-  }
   return (
     <main className={styles.main}>
       <div>
         TEST
-        <a onClick={handleRedirect}>TEST</a>
+        <a onClick={() => {
+    location.href = 'intent://comixest.com//#Intent;package=com.hustay.swing.p3cfded673ced4983b0db9399638730cb;scheme=https;end'
+  }}>TEST</a>
 
         <a href="https://universal-link-test-gamma.vercel.app"> APP DEEP LINK</a>
       </div>
