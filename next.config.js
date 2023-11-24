3b8d7193f491707d8cf5fa63ddf764b8d46ced2e
foyle <foyle@spacecloud.kr>
2023-11-24 13:45:54 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    headers() {
-      return [
-        {
-          source: "/.well-known/apple-app-site-association",
-          headers: [{ key: "content-type", value: "application/json" }]
-        }
-      ];
-    }
-  }
-}
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig
