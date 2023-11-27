9603fb7521019126b22120bf5684f69e3a5e865c
foyle <foyle@spacecloud.kr>
2023-11-27 14:54:38 +0900

    test

--- app/page.jsx
+++ app/page.jsx
@@ -36,9 +36,11 @@ export default function Home() {
     document.location.href = `twitter://search?query=%23hashtag`;
   
     // (2) 일정한 시간동안 앱이 실행되지 않으면 특정 기능을 실행하는 timeout을 생성한다 
+
+    var objPopup = window.open('about:blank','','width=100, height=100');
     timerRef.current = setTimeout(() => {
-      debugger;
-      document.location.href = `https://apps.apple.com/kr/app/%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%84%BC%ED%84%B0/id1526709791`
+      // debugger;
+      objPopup.location.href = `https://apps.apple.com/kr/app/%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%84%BC%ED%84%B0/id1526709791`
     }, 2000);
   };
 
@@ -61,13 +63,13 @@ export default function Home() {
 
           <h1>
             <a onClick={() => launchApp()}>Twitter</a>
-
-            
           </h1>
 
           <li>
           <a onClick={() => { document.location.href = `https://apps.apple.com/kr/app/%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%84%BC%ED%84%B0/id1526709791` }}>App Store</a>
           </li>
+
+          
 
           </li>
         
