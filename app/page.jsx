2d50a5da3507f051c856d73bb678f74be027e974
foyle <foyle@spacecloud.kr>
2023-11-27 13:45:07 +0900

    test

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,8 @@ export default function Home() {
     document.location.href = `twitter://search?query=%23hashtag`;
 
     timeRef.current = setTimeout(() => {
-      document.location.href = "https://apps.apple.com/kr/app/%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%84%BC%ED%84%B0/id1526709791";
+      // document.location.href = "https://apps.apple.com/kr/app/%EC%8A%A4%ED%8E%98%EC%9D%B4%EC%8A%A4%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%ED%98%B8%EC%8A%A4%ED%8A%B8%EC%84%BC%ED%84%B0/id1526709791";
+      document.location.href = `itms-apps://itunes.apple.com/app/1526709791`
     }, 2000);
   }
 
@@ -57,7 +58,10 @@ export default function Home() {
             <a href="http://guest-feature-chat-space.test.spacecloud.kr">chat-space</a>
           </li>
         </ul>
+
+        <button onClick={() => { window.open() }}>test</button>
         
+
         <a
             href="https://itunes.apple.com/kr/app/seupeiseukeullaudeu/id1025620281?l=en&amp;mt=8"
             target="_blank"
